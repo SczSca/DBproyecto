e0dc5312d655e64668ba4110fcd19be746cf5e7a
SczSca <aalvarez59@ucol.mx>
2022-11-29 16:10:53 -0600

    Implementacion para upSala y upCentro

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
 function validateForm(idx) {
-    if(idx === 1){
-        let uName = document.forms["login"]["username"].value;
-        let password = document.forms["login"]["password"].value;
-        if(!!uName && !!password){
+
+    if(idx === 2){
+        let name = document.forms["insertCentro"]["Nombre"].value;
+        let phone = document.forms["insertCentro"]["Telefono"].value;
+        let address = document.forms["insertCentro"]["Direccion"].value;
+        let email = document.forms["insertCentro"]["Email"].value;
+        if (!!name && !!phone && !!address && !!email) {
             return true;
         }else{
-            alert("valor faltante");
+            alert("Valor faltante"); 
             return false;
         }
-    }
-    else if(idx === 2){
-        let name = document.forms["insert"]["nombre"].value;
-        let descript = document.forms["insert"]["descripcion"].value;
-        let cant = document.forms["insert"]["cantidad"].value;
-        let precio = document.forms["insert"]["precio"].value;
-        if (!!name && !!descript && !!cant && !!precio) {
+        
+    }else if(idx === 21){
+        let name = document.forms["updateCentro"]["Nombre"].value;
+        let phone = document.forms["updateCentro"]["Telefono"].value;
+        let address = document.forms["updateCentro"]["Direccion"].value;
+        let email = document.forms["updateCentro"]["Email"].value;
+        if (!!name && !!phone && !!address && !!email) {
+            return true;
+        }else{
+            alert("Valor faltante"); 
+            return false;
+        }
+    }else if(idx === 3){
+        let name = document.forms["insertSala"]["Nombre"].value;
+        let tipo = document.forms["insertSala"]["Tipo"].value;
+        let descrip = document.forms["insertSala"]["Descripcion"].value;
+        let idCentro = document.forms["insertSala"]["IdCentro"].value;
+        if (!!name && !!tipo && !!descrip && !!idCentro) {
           return true;
         }else alert("Valor faltante"); return false;
-    }else if(idx === 3){
-
+    }else if(idx === 31){
+        let name = document.forms["updateSala"]["Nombre"].value;
+        let tipo = document.forms["updateSala"]["Tipo"].value;
+        let descrip = document.forms["updateSala"]["Descripcion"].value;
+        let idCentro = document.forms["updateSala"]["IdCentro"].value;
+        if (!!name && !!tipo && !!descrip && !!idCentro) {
+          return true;
+        }else alert("Valor faltante"); return false;
     }
-  }
-         
+}  
